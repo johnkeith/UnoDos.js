746bfdbbf347e639487ee1e049f9260a8a6b0fef
John Keith <johnkeith@outlook.com>
2014-06-11 17:35:34 -0400

    board methods placed in prototype and working

--- board.js
+++ board.js
@@ -1,45 +1,43 @@
 var Board = function() {
   this.board = [];
-
   this.letters = ["U","N","O","D","O",
                   "U","N","O","O","S",
                   "U","N","O","D","S",
                   "U","O","O","D","S",
                   "O","N","O","D","S"];
+};
 
-  this.shuffle_letters = function() {
-    this.letters.sort(function() {return 0.5 - Math.random();});
-  };
-
-  this.get_letter = function() {
-    return this.letters.splice(0,1);
-  };
-
-  this.build_board = function() {
-    for (var i = 0; i < 5; i++) {
-      row = [];
-      for( var j = 0; j < 5; j++) {
-        // row.push(new Tile)
-        row.push("J");
-      }
-      this.board.push(row);
+Board.prototype.build_board = function() {
+  for (var i = 0; i < 5; i++) {
+    row = [];
+    for( var j = 0; j < 5; j++) {
+      // row.push(new Tile)
+      row.push("J");
     }
-  };
+    this.board.push(row);
+  }
+};
 
-  this.board_full = function() {
-    var empty = true;
-    $.each(this.board, function(index, row) {
-      if (empty == false) {
-        return false
-      } else {
-        $.each(row, function(index, col) {
-          if (col == " ") {
-            empty = false;
-          };
-        });
-      }
-    });
-    return empty;
-  };
+Board.prototype.shuffle_letters = function() {
+  this.letters.sort(function() {return 0.5 - Math.random();});
+};
 
+Board.prototype.get_letter = function() {
+  return this.letters.splice(0,1);
+};
+
+Board.prototype.board_full = function() {
+  var empty = true;
+  $.each(this.board, function(index, row) {
+    if (empty == false) {
+      return false
+    } else {
+      $.each(row, function(index, col) {
+        if (col == " ") {
+          empty = false;
+        };
+      });
+    }
+  });
+  return empty;
 };
